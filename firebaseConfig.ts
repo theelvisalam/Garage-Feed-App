52a96239fc85ba6311ad0ace79101f62d4232523
Elvis Alam <alamelvism@gmail.com>
2025-05-09 01:05:32 -0500

    gitignore firebase keys

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,17 +1,9 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { firebaseKeys } from './firebaseKeys';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyCBE6yXks5O2b_49pFBz_jZate9VPtevf0',
-  authDomain: 'garagefeed-a0273.firebaseapp.com',
-  projectId: 'garagefeed-a0273',
-  storageBucket: 'garagefeed-a0273.appspot.com',
-  messagingSenderId: '192112542398',
-  appId: '1:192112542398:web:756607e946e45d760dd4ef',
-};
-
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseKeys);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
